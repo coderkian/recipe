7d508ad9c2115e1ec77270c9b43a0978c7437fad
kian <yongjianzhang86@gmail.com>
2014-09-04 22:22:46 +0800

    update

--- cstring/main.cpp
+++ cstring/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "cstring.hpp"
-#include "kianstring.hpp"
+#include "mystring.hpp"
 
 using namespace std;
 
 int main(int argc, char **grgv){
-	KianString str1("aaaa");
-	KianString str2("bbbb");
-	KianString str3 = str1+str2;
-	KianString str4 = "cccc";
+	MyString str1("aaaa");
+	MyString str2("bbbb");
+	MyString str3 = str1+str2;
+	MyString str4 = "cccc";
 	str4 += "dddd";
 	cout<<str1<<endl;
 	cout<<str2<<endl;
