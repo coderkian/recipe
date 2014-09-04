dd3bd8c8c9ce6b2cc7fe913844b73667ede5212c
kian <yongjianzhang86@gmail.com>
2014-09-05 00:14:27 +0800

    DCLP

--- design_pattern/singleton/singleton.cpp
+++ design_pattern/singleton/singleton.cpp
@@ -1,10 +1,20 @@
 #include "singleton.hpp"
 
-Singleton* Singleton::instance = nullptr;
+
+std::atomic<Singleton*> Singleton::instance;
+std::mutex Singleton::m_mutex;
 
 Singleton* Singleton::getInstance(){
-  if(nullptr == instance){
-    instance = new Singleton();
+  Singleton* tmp = instance.load(std::memory_order_relaxed);
+  std::atomic_thread_fence(std::memory_order_acquire);
+  if(nullptr == tmp){
+    std::lock_guard<std::mutex> lock(m_mutex);
+    tmp = instance.load(std::memory_order_relaxed);
+    if(nullptr == tmp){
+      tmp = new Singleton();
+      std::atomic_thread_fence(std::memory_order_release);
+      instance.store(tmp, std::memory_order_relaxed);
+    }
   }
   return instance;
 }
--- design_pattern/singleton/singleton.hpp
+++ design_pattern/singleton/singleton.hpp
@@ -1,10 +1,14 @@
 #ifndef SINGLETON_HPP
 #define SINGLETON_HPP
 
+#include <atomic>
+#include <mutex>
+
 class Singleton{
   public:
     static Singleton* getInstance();
   private:
-    static Singleton* instance;
+    static std::atomic<Singleton*> instance;
+    static std::mutex m_mutex;
 };
 #endif
